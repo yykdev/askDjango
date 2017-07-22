8808b57af7f5773bc9aab83b3e5d145ffdf4dcaa
yykdev <assavictory@gmail.com>
2017-07-22 16:43:38 +0900

    app settings include namespace

--- class04-08/askDjango_app/askDjango/urls.py
+++ class04-08/askDjango_app/askDjango/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ]
--- class04-08/askDjango_app/blog/urls.py
+++ class04-08/askDjango_app/blog/urls.py
@@ -7,9 +7,3 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls))
-    ]
